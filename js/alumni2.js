fb42359ae64363eecba979c4198618355730b05e
Hari Govind K K <45679986+harigovindkk@users.noreply.github.com>
2020-05-31 11:36:18 +0530

    updated alumni js

--- js/alumni2.js
+++ js/alumni2.js
@@ -1,8 +1,3 @@
-// Animations init
-new WOW().init();
-
-
-
 function op1()
 {
  login='<form class="text-center border border-light p-5" action="#!"><p class="h4 mb-4">Alumni Sign In</p><input type="email" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="E-mail"><input type="password" id="defaultLoginFormPassword" class="form-control mb-4" placeholder="Password"><div class="d-flex justify-content-around"><div><div class="custom-control custom-checkbox"><input type="checkbox" class="custom-control-input" id="defaultLoginFormRemember"><label class="custom-control-label" for="defaultLoginFormRemember">Remember me</label></div></div><div><a href="">Forgot password?</a></div></div><button class="btn btn-info btn-block my-4" type="submit">Sign in</button></form>';
